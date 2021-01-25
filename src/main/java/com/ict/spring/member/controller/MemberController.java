01fbbd9b38a41f559b77f1c6c7bb8018cbf02ee4
jaeyeun95 <jaeyeun95@gmail.com>
2021-01-26 00:02:46 +0900

    2021-01-26 MemberController 복습

--- src/main/java/com/ict/spring/member/controller/MemberController.java
+++ src/main/java/com/ict/spring/member/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.ict.spring.member.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ict.spring.member.model.vo.Member;
 
 @Controller	// Controller타입의 클래스로 선언해서 빈 스캐닝에 자동으로 등록시켜서 빈객체를 만들겠다.(이름을 지정하지 않으면 클래스명
 			// 앞에 글자를 소문자로해서 id가 만들어진다.
@@ -65,7 +67,52 @@ public class MemberController {
 	 * 
 	 * 		다만 어노테이션을 생략할 경우 defaultValue와 required설정 불가
 	 * 		없는 파라미터일 경우 null값이 입력
+	 */
+//	@RequestMapping(value="login.do", method=RequestMethod.POST)
+//	public String memberLogin(String id, String pwd) {
+//		System.out.println("ID : " + id);
+//		System.out.println("PWD : " + pwd);
+//		
+//		return "home";
+//	}
+	
+	/**
+	 *	4.	@ModelAttribute를 이용한 값 전달 방법
+	 *
+	 * 	요청 파라미터가 많은 경우 객체 타입으로 넘겨 받게 된다.
+	 * 	--> 기본생성자와 setter를 이용한 주입방식
+	 * 		꼭, 기본생성자와 setter생성자가 둘다 있어야한다.
 	 * 
+	 * 	이를 커맨드객체 방식이라고 한다.
+	 *	스프링 컨테이너가 기본생성자를 통해 Member객체를 생성하고
+	 *	setter메스드로 꺼낸 파라미터들로 값을 변경한 후에
+	 *	현재 이 메소드를 호출할 때 인자로 전달하며 호출하는 방식
+	 *	(주의 : 반드시 name 속성과 값과 필드명이 동일해야 한다, 또한 setter작성하는 규칙에 맞게 작성되어야 한다.)
 	 */
 	
+//	@RequestMapping(value="login.do", method=RequestMethod.POST)
+//	public String memberLogin(@ModelAttribute Member m) {
+//		System.out.println("ID : " + m.getId());
+//		System.out.println("PWD : " + m.getPwd());
+//		
+//		return "home";
+//	}
+	
+	/**
+	 * 5.	@ModelAttribute 어노테이션 생략하고 객체로 바로 전달하는 방법
+	 * 		어노테이션을 생략해도 자동으로 커맨드 객체로 매핑이된다.
+	 * 		생략 가능한 어노테이션이 많지만 가독성을 위해 보통 다 적어주는게 좋다.
+	 * 
+	 */
+	@RequestMapping(value="login.do",method=RequestMethod.POST)
+	public String memberLogin(Member m) {
+		System.out.println("ID : " + m.getId());
+		System.out.println("PWD : " + m.getPwd());
+		
+		return "home";
+	}
+	
 }
+
+
+
